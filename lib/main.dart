fff01590eacb4274bc5bc2c42d043e1ad8e5a56a
agrahafiz13 <agrahafiz2@gmail.com>
2026-02-28 15:34:10 +0700

    menambahkan halaman keranjang setelah menambahkan item

--- lib/main.dart
+++ lib/main.dart
@@ -104,3 +104,47 @@ class AddButton extends StatelessWidget {
     );
   }
 }
+
+//Halaman Keranjang
+class MyCart extends StatelessWidget {
+  const MyCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var cart = context.watch<CartModel>();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Keranjang Belanja')),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: const Icon(Icons.fastfood),
+                  title: Text(cart.items[index]),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: ElevatedButton(
+              onPressed: cart.items.isEmpty
+                  ? null
+                  : () {
+                      cart.removeAll();
+                    },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+              child: const Text(
+                'Hapus Keranjang',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
