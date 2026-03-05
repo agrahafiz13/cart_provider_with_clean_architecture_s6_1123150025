57adadb9186d235f80dd5e9e248965b3df622ce9
agrahafiz13 <agrahafiz2@gmail.com>
2026-03-06 01:05:49 +0700

    membersihkan main dengan membuat file injection

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 import 'core/routes/app_router.dart';
-import 'features/cart/data/repositories/cart_repository_impl.dart';
-import 'features/cart/presentation/providers/cart_provider.dart';
+import 'injection.dart';
 
-void main() {
-  runApp(
-    ChangeNotifierProvider(
-      create: (_) {
-        final repository = CartRepositoryImpl();
-        return CartProvider(repository);
-      },
-      child: const MyApp(),
-    ),
-  );
-}
+void main() => runApp(buildApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
